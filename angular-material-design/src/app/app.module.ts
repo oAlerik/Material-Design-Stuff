57d73b4957deee0440ee6e9dd99818ea550962a7
Jonny <jonathan44472@gmail.com>
2020-01-09 18:06:13 +0000

    Menu and routing works

--- angular-material-design/src/app/app.module.ts
+++ angular-material-design/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatListModule } from "@angular/material/list";
 import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HomepageComponent } from './homepage/homepage.component';
-import { InfoCardComponent } from './info-card/info-card.component';
+import { HomepageComponent } from "./homepage/homepage.component";
+import { InfoCardComponent } from "./info-card/info-card.component";
 
 @NgModule({
   declarations: [AppComponent, HomepageComponent, InfoCardComponent],
@@ -16,6 +18,8 @@ import { InfoCardComponent } from './info-card/info-card.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
     FormsModule
   ],
   providers: [],
