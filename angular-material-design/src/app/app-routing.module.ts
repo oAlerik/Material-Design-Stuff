d9cccfda687ec1ad9c33864bef5ba0a18aed72dd
Jonny <jonathan44472@gmail.com>
2020-01-09 17:57:53 +0000

    Routing added kinda

--- angular-material-design/src/app/app-routing.module.ts
+++ angular-material-design/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { AppComponent } from "./app.component";
+import { HomepageComponent } from "./homepage/homepage.component";
+import { InfoCardComponent } from "./info-card/info-card.component";
 
-const routes: Routes = [{ path: "test", component: AppComponent }];
+const routes: Routes = [
+  { path: "", component: HomepageComponent },
+  { path: "info-card", component: InfoCardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-material-design/src/app/app.module.ts
+++ angular-material-design/src/app/app.module.ts
@@ -6,9 +6,11 @@ import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HomepageComponent } from './homepage/homepage.component';
+import { InfoCardComponent } from './info-card/info-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomepageComponent, InfoCardComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
